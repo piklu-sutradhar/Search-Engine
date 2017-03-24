b615af4b101586ae2e78f826b2315ea53e9e154b
Piklu Sutradhar <Piklu Sutradhar>
2017-03-23 21:53:11 -0500

    created test.c
    A test framework to test enigine.c file

--- test.c
+++ test.c
@@ -4,8 +4,10 @@
 #include <assert.h>
 #include "table.h"
 #include "engine.h"
+#include "levenshtein.c/levenshtein.h"
 
-typedef enum BOOL { false, true } Boolean;
+typedef enum BOOL { false,
+                    true } Boolean;
 #define LENGTH 8
 static int testsFailed = 0;
 static int testsExecuted = 0;
@@ -13,42 +15,86 @@ static int testsExecuted = 0;
 //*****************
 // Prototypes
 //*****************
-
+static char input[] = "HELLO";
+static char *similar;
 void testBuildTable();
-void testPager();
-void testToLower( char * const input);
+void testToLower(char *const input);
 void testDeleteTable();
 
 int main(int argc, char const *argv[])
 {
-    FILE *dict;
-	char input[]= "Hello";
-    char similar[MAX];
-    dict = fopen("sample.txt", "r");
-    if(dict != NULL){
-        buildTable(dict,input,similar);
-        printf("Input is %s and The similar word is %s\n", input, similar);
-        if (size() <= 0){
-
-        }
-    }
-
+    testBuildTable();
+    testToLower(char *input);
+    testDeleteTable();
     return EXIT_SUCCESS;
 }
-void testBuildTable(){
-
-}
-void testToLower( char * input){
-    Boolean result = true;
-    toLower(input);
-    for(i = 0; i < strlen(input) && result; i++){
-        if(*(inpput + i) != tolower(*(inpput + i))){
-            result = false;
+void testBuildTable()
+{
+    FILE *dict;
+    dict = fopen("sample.txt", "r");
+    if (dict != NULL)
+    {
+        buildTable(dict, input, similar);
+        printf("Input is %s and The similar word is %s\n", input, similar);
+        if (size() <= 0)
+        {
+            printf("Table built failed");
+            testsFailed++;
+            testExecuted++;
         }
+        else
+        {
+            int dist;
+            char *result = firstItem();
+            //printf("%s\n", result );
+
+            while (result != NULL)
+            {
+                dist = levenshtein(input, result);
+                if (dist < 5)
+                {
+                    printf("Success: The distance is %d\n", dist);
+                }
+                else
+                {
+                    printf("Failed: The distance >= 5");
+                    testsFailed++;
+                }
+                result = nextItem();
+                testsExecuted++;
+            }
+        }
+        fclose(dict);
     }
-    if(!result){
-        printf("Test Failed");
-        testsFailed++;
+    void testToLower(char *input)
+    {
+        Boolean result = true;
+        toLower(input);
+        for (i = 0; i < strlen(input) && result; i++)
+        {
+            if (*(inpput + i) != tolower(*(inpput + i)))
+            {
+                result = false;
+            }
+        }
+        if (!result)
+        {
+            printf("Test Failed");
+            testsFailed++;
+        }
+        testsExecuted++;
     }
-    testsExecuted++;
-}
+    void testDeleteTable()
+    {
+        deleteTable();
+        if (size() == 0)
+        {
+            printf("SUCCESS : Table deleted ");
+        }
+        else
+        {
+            printf("FAILED: Table is not deleted entirely");
+            testsFailed++;
+        }
+        testsExcuted++;
+    }
